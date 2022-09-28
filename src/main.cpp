f47dd40ea856af299403105c12b9ba003024eab8
Minh Nguyen <retrominh@gmail.com>
2022-09-28 16:34:45 -0700

    adding base framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <Comms.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+Task taskTable[] = {
+
+};
+
+#define TASK_COUNT (sizeof(taskTable) / sizeof (struct Task))
+
+int main() {
+
+    while(1) {
+        for(uint32_t i = 0; i < TASK_COUNT; i++) { // for each task, execute if next time >= current time
+            uint32_t ticks = micros(); // current time in microseconds
+            if (taskTable[i].nexttime - ticks > UINT32_MAX / 2 && taskTable[i].enabled) {
+                taskTable[i].nexttime = ticks + taskTable[i].taskCall();
+            }
+        }
+        Comms::processWaitingPackets();
+    }
+    return 0;
 }
