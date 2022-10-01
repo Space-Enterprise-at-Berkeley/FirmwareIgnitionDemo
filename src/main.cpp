43c0c607bd389e04c8533883a6218e3b3c286766
justinwang066 <justinwang066@gmail.com>
2022-09-30 20:31:07 -0700

    push tc

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main() {
 
     while(1) {
         for(uint32_t i = 0; i < TASK_COUNT; i++) { // for each task, execute. if next time >= current time
-            uint32_t ticks = micros(); // current time in microseconds
+            uint32_t ticks = micros(); // current time in microseconds.
             if (taskTable[i].nexttime - ticks > UINT32_MAX / 2 && taskTable[i].enabled) {
                 taskTable[i].nexttime = ticks + taskTable[i].taskCall();
             }
