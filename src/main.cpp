8f84d420f4f083945f2e35d60a2f4c1d1c5c7adb
justinwang066 <justinwang066@gmail.com>
2022-09-30 20:27:07 -0700

    tc read

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main() {
     Thermocouples::init();
 
     while(1) {
-        for(uint32_t i = 0; i < TASK_COUNT; i++) { // for each task, execute if next time >= current time
+        for(uint32_t i = 0; i < TASK_COUNT; i++) { // for each task, execute. if next time >= current time
             uint32_t ticks = micros(); // current time in microseconds
             if (taskTable[i].nexttime - ticks > UINT32_MAX / 2 && taskTable[i].enabled) {
                 taskTable[i].nexttime = ticks + taskTable[i].taskCall();
